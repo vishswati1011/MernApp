bcc854b0071d715ed7e7440c3fe6ffff408627d7
vishswati1011 <sati.v1011@gmail.com>
2021-10-26 10:59:23 +0530

    post request using async await

--- src/app.js
+++ src/app.js
@@ -7,23 +7,39 @@ const app =express();
 const port =process.env.PORT || 4000;
 
 app.use(express.json())  //this methed is use for get request in json 
-// create a new student                          
-app.post('/students',(req,res)=>{
 
-    // const user=new  Student({
-    //         "name":"swati vishwakarma",
-    //         "email":"swati@gmail.com",
-    //         "phone":8457123654,
-    //         "address":"Indore"   
-    // })
-    console.log(req.body);
-    const user=new  Student(req.body);
-    //promise example
-    user.save().then(()=>{
-        res.status(201).send(user);
-    }).catch((e)=>{
+// create a new student  
+//post reqest using                        
+// app.post('/students',(req,res)=>{
+
+//     // we are having these below object inside req
+//     // const user=new  Student({
+//     //         "name":"swati vishwakarma",
+//     //         "email":"swati@gmail.com",
+//     //         "phone":8457123654,
+//     //         "address":"Indore"   
+//     // })
+//     console.log(req.body);
+//     const user=new  Student(req.body);
+//     //promise example
+//     user.save().then(()=>{
+//         res.status(201).send(user);
+//     }).catch((e)=>{
+//         res.status(400).send(e);
+//     })          
+// })
+
+//post reqest using async await 
+app.post('/students',async(req,res)=>{
+
+    try{
+        const user = new  Student(req.body);  
+        const createUser = await user.save();
+        res.status(201).send(createUser);
+    }catch(e){
         res.status(400).send(e);
-    })          
+       
+    }
 })
 
 
